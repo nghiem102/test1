276ddc7997fa1e836492220e04bd84a86abd815d
nghiem102 <ledinhnghiem102@gmail.com>
2022-04-13 22:16:38 +0700

    update

--- src/data/Post.js
+++ src/data/Post.js
@@ -1,8 +1,10 @@
 import htmlIcon from '../img/html-icon 1.png'
+import CssIcon from '../img/css-icon 1.png'
+import UrlIcon from '../img/url-icon 1.png'
 const posts = [
     {
         title: 'Lorem ipsum dolor sit amet',
-        img: htmlIcon,
+        img: CssIcon,
         desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non dui sodales, faucibus libero ut, posuere felis. Donec imperdiet suscipit accumsan. Aenean consequat condimentum velit ut tempor. Nam porta massa in metus bibendum congue. Pellentesque ultrices liquam egestas nunc at ullamcorper ultricies. Donec feugiat velit nulla, vel sodales est ullamcorper id. Aenean consequat condimentum velit ut tempor. Nam porta massa in metus bibendum congue. Pellentesque ultrices vestibulum mattis.'
     },
     {
@@ -12,7 +14,7 @@ const posts = [
     },
     {
         title: 'Lorem ipsum dolor sit amet',
-        img: htmlIcon,
+        img: UrlIcon,
         desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non dui sodales, faucibus libero ut, posuere felis. Donec imperdiet suscipit accumsan. Aenean consequat condimentum velit ut tempor. Nam porta massa in metus bibendum congue. Pellentesque ultrices liquam egestas nunc at ullamcorper ultricies. Donec feugiat velit nulla, vel sodales est ullamcorper id. Aenean consequat condimentum velit ut tempor. Nam porta massa in metus bibendum congue. Pellentesque ultrices vestibulum mattis.'
     }
 ]
